c7db0dee3feaa93cd61c64c7b3c0c9a432d1dfe2
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-07-27 22:40:52 -0400

    Added funtionality to allow the user to respond to the question, logging the answer to the console after hitting the start button as a test

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Complete your work inside of `code.js`
 
 Follow the instructions provided on my.kenzie.academy for this assignment.
 - Week 26 Topic Session 
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+- https://www.w3schools.com/tags/tag_input.asp
+- https://www.w3schools.com/js/js_htmldom_nodes.asp
--- code.js
+++ code.js
@@ -12,7 +12,7 @@ async function randomAPIQuestion() { // Retrieve a random question's whose clue 
         .then(exportCategoryData => retrieveAPIQuestions(exportCategoryData));
 }
 
-async function retrieveAPIQuestions(randomCategoryData) { // Retrieve up to 100 Questions from jService Kenzie API limited to a Category ID
+async function retrieveAPIQuestions(randomCategoryData) { // Retrieve up to 100 Questions from jService Kenzie API limited to the Category ID returned from the randomAPIQuestion function
     let setCategoryId = randomCategoryData.categoryId;
     fetch(`https://jservice.kenzie.academy/api/clues?category=${setCategoryId}`)
         .then(setCategoryResponse => setCategoryResponse.json())
@@ -24,13 +24,27 @@ randomAPIQuestion();
 
 function startGame() { // Starts the game
     startButton.remove();
+    let submitAnswer = document.getElementById("submitAnswer");
+    submitAnswer.addEventListener("click", checkUserAnswer);
 }
 
-
+function checkUserAnswer() { // Compare user answer to correct answer
+    let answer = document.getElementById("userAnswer").value
+    console.log(answer);
+}
 
 function renderGameQuestion(questionData) {// Display one of the Questions from the random category with an Answer Field
     let question = selectedQuestion(questionData);
-    document.write(question);
+    let gameArea = document.getElementById("gameArea");
+    let answerField = document.createElement("input");
+    let submitAnswer = document.createElement("button");
+    gameArea.append(question);
+    gameArea.append(answerField);
+    answerField.type = "text";
+    answerField.id = "userAnswer";
+    gameArea.append(submitAnswer);
+    submitAnswer.id = "submitAnswer";
+    submitAnswer.innerText = "Check Answer";
     return questionData;
 }
 
@@ -41,7 +55,7 @@ function selectedQuestion(dataSet) { // This function will take in the set of qu
     return JSON.stringify(result.clues[arrayIndex].question)
 }
 
-// Compare user answer to correct answer
+
 
 // Proceed to next question after answered
 
