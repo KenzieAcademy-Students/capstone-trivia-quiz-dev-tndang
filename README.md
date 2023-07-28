56825437602a85952b0cae57785bc651ba031c1c
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-07-28 03:35:04 +0000

    deploy: 12559ba791c0a13536fd85ff27e7d92d3991c4d0

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Complete your work inside of `code.js`
 
 Follow the instructions provided on my.kenzie.academy for this assignment.
 - Week 26 Topic Session 
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+- https://www.w3schools.com/tags/tag_input.asp
+- https://www.w3schools.com/js/js_htmldom_nodes.asp
+- https://stackoverflow.com/questions/13020246/remove-special-symbols-and-extra-spaces-and-replace-with-underscore-using-the-re
+- https://www.freecodecamp.org/news/javascript-refresh-page-how-to-reload-a-page-in-js/#:~:text=The%20simplest%20way%20to%20refresh,and%20loading%20the%20latest%20content.
--- code.js
+++ code.js
@@ -6,13 +6,20 @@
 let startButton = document.getElementById("startButton");
 startButton.addEventListener("click", startGame);
 
+function startGame() { // Starts the game
+    startButton.remove();
+    console.log(`Score: ${userScore}`);
+    let submitAnswer = document.getElementById("submitAnswer");
+    submitAnswer.addEventListener("click", checkUserAnswer);
+}
+
 async function randomAPIQuestion() { // Retrieve a random question's whose clue is valid from the jService Kenzie API
     fetch (`https://jservice.kenzie.academy/api/random-clue?valid=true`)
         .then(randomQuestionResponse => randomQuestionResponse.json())
         .then(exportCategoryData => retrieveAPIQuestions(exportCategoryData));
 }
 
-async function retrieveAPIQuestions(randomCategoryData) { // Retrieve up to 100 Questions from jService Kenzie API limited to a Category ID
+async function retrieveAPIQuestions(randomCategoryData) { // Retrieve up to 100 Questions from jService Kenzie API limited to the Category ID returned from the randomAPIQuestion function
     let setCategoryId = randomCategoryData.categoryId;
     fetch(`https://jservice.kenzie.academy/api/clues?category=${setCategoryId}`)
         .then(setCategoryResponse => setCategoryResponse.json())
@@ -22,17 +29,47 @@ async function retrieveAPIQuestions(randomCategoryData) { // Retrieve up to 100 
 
 randomAPIQuestion();
 
-function startGame() { // Starts the game
-    startButton.remove();
+let currentQuestionAnswer;
+let userScore = 0;
+
+function selectedQuestion(dataSet) { // This function will take in the set of questions returned from the API and select a question to be rendered from that set
+    console.log(dataSet);
+    let result = dataSet;
+    let arrayIndex = Math.floor(Math.random(0) * result.clues.length - 1);
+    currentQuestionAnswer = JSON.stringify(result.clues[arrayIndex].answer);
+    return JSON.stringify(result.clues[arrayIndex].question)
+}
+
+function checkUserAnswer() { // Compare user answer to correct answer
+    let userAnswer = document.getElementById("userAnswer").value.replace(/[^A-Z0-9]/ig, "");
+    let finalUserAnswerLC = userAnswer.toLowerCase();
+    let correctAnswer = currentQuestionAnswer.replace(/[^A-Z0-9]/ig, "")
+    let correctAnswerLC = correctAnswer.toLowerCase();
+    console.log(finalUserAnswerLC);
+    console.log(correctAnswerLC);
+    if (finalUserAnswerLC === correctAnswerLC) {
+        userScore++;
+        console.log("Nice Job")
+        console.log(`Score: ${userScore}`)
+    } else {
+        console.log("Sorry you are wrong...")
+    }   
 }
 
 function renderGameQuestion(questionData) {// Display one of the Questions from the random category with an Answer Field
-    console.log(questionData);
-    document.write(questionData);
+    let question = selectedQuestion(questionData);
+    let gameArea = document.getElementById("gameArea");
+    let answerField = document.createElement("input");
+    let submitAnswer = document.createElement("button");
+    gameArea.append(question);
+    gameArea.append(answerField);
+    answerField.type = "text";
+    answerField.id = "userAnswer";
+    gameArea.append(submitAnswer);
+    submitAnswer.id = "submitAnswer";
+    submitAnswer.innerText = "Check Answer";
     return questionData;
 }
-
-// Compare user answer to correct answer
 
 // Proceed to next question after answered
 
