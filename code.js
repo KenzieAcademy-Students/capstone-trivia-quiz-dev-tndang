f0cbe083238914ac53642a312e397a5f3300d10d
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-07-29 03:08:39 +0000

    deploy: bc38711c2e3fc0a7e1aa1feddcc84434b57cb2db

--- code.js
+++ code.js
@@ -6,10 +6,13 @@
 let startButton = document.getElementById("startButton");
 startButton.addEventListener("click", startGame);
 
-function startGame() { // Starts the game
-    startButton.remove();
-    console.log(`Score: ${userScore}`);
+function startGame() { // Starts the game 
+    let welcomeScreen = document.getElementById("gameWelcome");
     let submitAnswer = document.getElementById("submitAnswer");
+    let gameScreen = document.getElementById("gameArea");
+    welcomeScreen.style.display = "none";
+    gameScreen.style.display = "block"
+    console.log(`Score: ${userScore}`);
     submitAnswer.addEventListener("click", checkUserAnswer);
 }
 
@@ -30,17 +33,23 @@ async function retrieveAPIQuestions(randomCategoryData) { // Retrieve up to 100 
 randomAPIQuestion();
 
 let currentQuestionAnswer;
+let currentQuestionIndex;
 let userScore = 0;
 
 function selectedQuestion(dataSet) { // This function will take in the set of questions returned from the API and select a question to be rendered from that set
     console.log(dataSet);
     let result = dataSet;
     let arrayIndex = Math.floor(Math.random(0) * result.clues.length - 1);
+    currentQuestionIndex = arrayIndex;
     currentQuestionAnswer = JSON.stringify(result.clues[arrayIndex].answer);
     return JSON.stringify(result.clues[arrayIndex].question)
 }
 
 function checkUserAnswer() { // Compare user answer to correct answer
+    let congratsScreen = document.getElementById("congratsMessage");
+    let gameOverScreen = document.getElementById("gameOverMessage");
+    let nextButton = document.getElementById("nextButton");
+    let restartButton = document.getElementById("restartButton");
     let userAnswer = document.getElementById("userAnswer").value.replace(/[^A-Z0-9]/ig, "");
     let finalUserAnswerLC = userAnswer.toLowerCase();
     let correctAnswer = currentQuestionAnswer.replace(/[^A-Z0-9]/ig, "")
@@ -49,16 +58,29 @@ function checkUserAnswer() { // Compare user answer to correct answer
     console.log(correctAnswerLC);
     if (finalUserAnswerLC === correctAnswerLC) {
         userScore++;
+        congratsScreen.style.display = "block";
+        nextButton.addEventListener("click", nextQuestion);
         console.log("Nice Job")
         console.log(`Score: ${userScore}`)
     } else {
+        gameOverScreen.style.display = "block";
+        restartButton.addEventListener("click", restartGame)
         console.log("Sorry you are wrong...")
     }   
 }
 
+function nextQuestion() {
+    let congratsScreen = document.getElementById("congratsMessage");
+    congratsScreen.style.display = "none";
+}
+
+function restartGame() {
+    location.reload();
+}
+
 function renderGameQuestion(questionData) {// Display one of the Questions from the random category with an Answer Field
     let question = selectedQuestion(questionData);
-    let gameArea = document.getElementById("gameArea");
+    let gameArea = document.getElementById("gameContent");
     let answerField = document.createElement("input");
     let submitAnswer = document.createElement("button");
     gameArea.append(question);
